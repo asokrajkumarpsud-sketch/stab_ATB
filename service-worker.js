350176044d547f00bf5d9e9c553795e34f193b99
asokrajkumarpsud-sketch <asok.rajkumar.psud@gmail.com>
2026-04-14 13:42:33 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,37 @@
-const CACHE_NAME = "stabilite-atb-v2";
+const CACHE_NAME = "stabilite-atb-v3";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./data.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+"/",
+"/index.html",
+"/data.js",
+"/manifest.json",
+"/icon-192.png",
+"/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(async cache => {
-      try {
-        await cache.addAll(urlsToCache);
-        self.skipWaiting();
-      } catch (err) {
-        console.error("Cache error:", err);
-      }
-    })
-  );
+self.skipWaiting();
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+);
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+event.waitUntil(
+caches.keys().then(keys => {
+return Promise.all(
+keys
+.filter(k => k !== CACHE_NAME)
+.map(k => caches.delete(k))
+);
+})
+);
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).catch(() => {
-        return caches.match("./index.html");
-      });
-    })
-  );
+event.respondWith(
+caches.match(event.request)
+.then(response => response || fetch(event.request))
+);
 });
